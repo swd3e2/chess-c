ac57ed4dc53a8cc106d935f3dcac352d4c4f8e86
Тарасиди Александр Георгиевич <aleksandr.tarasidi@sbermegamarket.ru>
2024-05-01 14:05:33 +0500

    Initial Commit

---
+++ game/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  game
+//
+//  Created by Александр Тарасиди on 01.05.2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
