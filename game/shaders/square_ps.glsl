dab2cb8f372739624dfd0595c874e9450f02b328
Тарасиди Александр Георгиевич <aleksandr.tarasidi@sbermegamarket.ru>
2024-05-15 14:37:48 +0500

    Начало реализации хождения фигур

--- game/shaders/square_ps.glsl
+++ game/shaders/square_ps.glsl
@@ -1,15 +1,18 @@
 #version 330
 
 uniform int is_hovered;
+uniform int possible_move;
 
 out vec4 frag_color;
 
 in vec4 color;
 
 void main() {
-  if (is_hovered == 0) {
-    frag_color = color;
-  } else {
+  if (is_hovered == 1) {
     frag_color = vec4(1.0f, 0.0f, 1.0f, 1.0f);
+  } else if (possible_move == 1) {
+    frag_color = color + vec4(0.0f, 0.3f, 0.0f, 1.0f);
+  } else {
+    frag_color = color;
   }
 }
